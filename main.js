6c932a77c1d75ca3447b29344434134b0701cdea
sakumashirayuki <2909549511@qq.com>
2021-04-27 17:26:26 +0800

    add Dijkstra

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 class Vex{
     constructor(value){
         this.data = value;
-        this.edge = [];// use adjacent matrix, contain the index
+        this.edge = [];// edge list: [vex, weight]
+        this.dist = Number.MAX_SAFE_INTEGER;
+        this.path = null; // the previous vertex on the path
+        this.known = false;
         this.in = 0; // the indegree of the vex
     }
 }
@@ -12,47 +15,75 @@ class Graph{
         for(const edge of vr){
             const v1 = v.indexOf(edge[0]); // from vertex1 to vertex2
             const v2 = v.indexOf(edge[1]); // v1 is the index of vertex1, v2 is the index of vertex2
-            vexs[v1].edge.push(vexs[v2]); // a better way is to add the vertex object to it
+            vexs[v1].edge.push([vexs[v2], edge[2]]); 
             vexs[v2].in++;
         }
         this.adjmatrix = vexs;
+        this.n = this.adjmatrix.length;
     }
-}
 
-const findNewVertexOfIndegreeZero = (G)=>{
-    let zeroInVertex = null;
-    const vexNumber = G.adjmatrix.length;
-    for(let i = 0; i < vexNumber; i++){
-        if(G.adjmatrix[i]&&G.adjmatrix[i].in===0){ // some vertex has been deleted
-            zeroInVertex = G.adjmatrix[i]; // copy this vertex instance
-            break;
+    unweight(s){ // s is a vex
+        const queue  = [];
+        s.dist = 0;
+        queue.push(s);
+        while(queue.length){
+            let currentVex = queue.shift();
+            let currentDist = currentVex.dist;
+            for(const adjVex of currentVex.edge){
+                if(adjVex.dist==Number.MAX_SAFE_INTEGER){ // means haven't visited
+                    adjVex.dist = currentDist + 1;
+                    queue.push(adjVex);
+                }
+            }
         }
     }
-    return zeroInVertex;
-}
 
-const topsort = (G)=>{
-    const n = G.adjmatrix.length; // number of vertex
-    const sortArray = []; 
-    const queue = []; // a queue to store all the vertex that indegree is 0
-    for(vertex of G.adjmatrix){
-        if(vertex.in===0)
-            queue.push(vertex);
+    minDistance(){ // return the index of the smallest unknown distance vertex
+        // O(N), because we don't have built-in priority queue
+        let min = Number.MAX_SAFE_INTEGER;
+        let minIndex;
+        for(let i = 0; i < this.n; i++){
+            if(this.adjmatrix[i].dist <= min && !this.adjmatrix[i].known){
+                minIndex = i;
+                min = this.adjmatrix[i].dist;
+            }
+        }
+        return minIndex;
     }
-    while(queue.length){
-        const curVertex = queue.shift();
-        sortArray.push(curVertex.data);
-        for(const adjacentV of curVertex.edge){ // adjacentV is the vertex object
-            // console.log(adjacentV);
-                adjacentV.in--;
-                if(adjacentV.in===0)
-                    queue.push(adjacentV);
+
+    dijsktra(s){ // s is a vex
+        s.dist = 0; // the starting vertex
+        for(let i = 0; i < this.n - 1; i++){ // we don't calculate the starting vertex
+            const currentVexIndex = this.minDistance();
+            // console.log(currentVexIndex);
+            const currentVex = this.adjmatrix[currentVexIndex];
+            currentVex.known = true;
+            for(const [adjVex, weight] of currentVex.edge){
+                if(!adjVex.known){
+                    if((currentVex.dist + weight) < adjVex.dist){
+                        // update the dist
+                        adjVex.dist = currentVex.dist + weight;
+                        // add to path
+                        adjVex.path = currentVex;
+                    }
+                }
+            }
         }
     }
-    return sortArray;
+
+    printPath(v){// recursive print the path
+        if(v==null)
+            return;
+        this.printPath(v.path);
+        console.log("to");
+        console.log(v.data);
+    }
 }
 
 const values = [1,2,3,4,5,6,7];
-const edges = [[1,2],[1,3],[1,4],[2,4],[2,5],[3,6],[4,3],[4,6],[4,7],[5,4],[5,7],[7,6]];
+// egdes: vex1, vex2, distance
+const edges = [[1,2,2],[1,4,1],[2,4,3],[2,5,10],[3,1,4],[3,6,5],[4,3,2],[4,5,2],[4,6,8],[4,7,4],[5,7,6],[7,6,1]];
 const myGraph = new Graph(values, edges);
-console.log(topsort(myGraph));
+myGraph.dijsktra(myGraph.adjmatrix[0]); // start point is the vex with value of 1
+// console.log(myGraph);
+myGraph.printPath(myGraph.adjmatrix[6]);
