b4e66acf97ce2ca5961e96e95d98a834ec6d6400
sakumashirayuki <2909549511@qq.com>
2021-04-28 15:42:36 +0800

    update

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ class Vex{
         this.edge = [];// edge list: [vex, weight]
         this.dist = Number.MAX_SAFE_INTEGER;
         this.path = null; // the previous vertex on the path
-        this.known = false;
         this.in = 0; // the indegree of the vex
     }
 }
@@ -22,50 +21,19 @@ class Graph{
         this.n = this.adjmatrix.length;
     }
 
-    unweight(s){ // s is a vex
-        const queue  = [];
+    weightedNegative(s){ // s is a vex
         s.dist = 0;
+        const queue = [];
         queue.push(s);
         while(queue.length){
-            let currentVex = queue.shift();
-            let currentDist = currentVex.dist;
-            for(const adjVex of currentVex.edge){
-                if(adjVex.dist==Number.MAX_SAFE_INTEGER){ // means haven't visited
-                    adjVex.dist = currentDist + 1;
-                    queue.push(adjVex);
-                }
-            }
-        }
-    }
-
-    minDistance(){ // return the index of the smallest unknown distance vertex
-        // O(N), because we don't have built-in priority queue
-        let min = Number.MAX_SAFE_INTEGER;
-        let minIndex;
-        for(let i = 0; i < this.n; i++){
-            if(this.adjmatrix[i].dist <= min && !this.adjmatrix[i].known){
-                minIndex = i;
-                min = this.adjmatrix[i].dist;
-            }
-        }
-        return minIndex;
-    }
-
-    dijsktra(s){ // s is a vex
-        s.dist = 0; // the starting vertex
-        for(let i = 0; i < this.n - 1; i++){ // we don't calculate the starting vertex
-            const currentVexIndex = this.minDistance();
-            // console.log(currentVexIndex);
-            const currentVex = this.adjmatrix[currentVexIndex];
-            currentVex.known = true;
+            const currentVex = queue.shift();
             for(const [adjVex, weight] of currentVex.edge){
-                if(!adjVex.known){
-                    if((currentVex.dist + weight) < adjVex.dist){
-                        // update the dist
-                        adjVex.dist = currentVex.dist + weight;
-                        // add to path
-                        adjVex.path = currentVex;
-                    }
+                if(currentVex.dist + weight < adjVex.dist){
+                    adjVex.dist = currentVex.dist + weight;
+                    adjVex.path = currentVex;
+                }
+                if(!queue.find((ele)=>ele==adjVex)){ // not in the queue
+                    queue.push(adjVex);
                 }
             }
         }
@@ -80,10 +48,9 @@ class Graph{
     }
 }
 
-const values = [1,2,3,4,5,6,7];
+const values = ['A', 'B', 'C', 'D', 'E'];
 // egdes: vex1, vex2, distance
-const edges = [[1,2,2],[1,4,1],[2,4,3],[2,5,10],[3,1,4],[3,6,5],[4,3,2],[4,5,2],[4,6,8],[4,7,4],[5,7,6],[7,6,1]];
+const edges = [['A','B',210],['A','D',100],['A','E',20],['B','C',20],['B','D',-150],['C','E',10],['D','C',-30],['D','E',60]];
 const myGraph = new Graph(values, edges);
-myGraph.dijsktra(myGraph.adjmatrix[0]); // start point is the vex with value of 1
-// console.log(myGraph);
-myGraph.printPath(myGraph.adjmatrix[6]);
+myGraph.weightedNegative(myGraph.adjmatrix[0]); // start point is the vex with value of 1
+myGraph.printPath(myGraph.adjmatrix[4]);
