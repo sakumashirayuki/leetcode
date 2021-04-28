c12311e689fe4dadf58580e720054ebcc348e26f
sakumashirayuki <2909549511@qq.com>
2021-04-28 16:45:34 +0800

    add prim

--- main.js
+++ main.js
@@ -2,17 +2,18 @@ class Vex{
     constructor(value){
         this.data = value;
         this.edge = [];// edge list: [vex, weight]
-        this.dist = Number.MAX_SAFE_INTEGER;
-        this.path = null; // the previous vertex on the path
+        this.dist = Number.MAX_SAFE_INTEGER; // min weight
+        this.path = null; // the vertex connecting to it
+        this.known = false;
         this.in = 0; // the indegree of the vex
     }
 }
 
-class Graph{
+class Graph{ // undirected graph
     constructor(v, vr){ // v is the value array, vr is the edge array
         const vexs = new Array(v.length).fill(null).map((_,id)=>new Vex(v[id])); // v and vexs have corresponding index
         for(const edge of vr){
-            const v1 = v.indexOf(edge[0]); // from vertex1 to vertex2
+            const v1 = v.indexOf(edge[0]); // 
             const v2 = v.indexOf(edge[1]); // v1 is the index of vertex1, v2 is the index of vertex2
             vexs[v1].edge.push([vexs[v2], edge[2]]); 
             vexs[v2].in++;
@@ -21,36 +22,46 @@ class Graph{
         this.n = this.adjmatrix.length;
     }
 
-    weightedNegative(s){ // s is a vex
-        s.dist = 0;
-        const queue = [];
-        queue.push(s);
-        while(queue.length){
-            const currentVex = queue.shift();
+    minDistance(){ // return the index of the smallest unknown distance vertex
+        // O(N), because we don't have built-in priority queue
+        let min = Number.MAX_SAFE_INTEGER;
+        let minIndex;
+        for(let i = 0; i < this.n; i++){
+            if(this.adjmatrix[i].dist <= min && !this.adjmatrix[i].known){
+                minIndex = i;
+                min = this.adjmatrix[i].dist;
+            }
+        }
+        return minIndex;
+    }
+
+    prim(){ // Minimum spanning tree, prim method
+        for(let i = 0; i < this.n; i++){
+            const currentVexIndex = this.minDistance();
+            const currentVex = this.adjmatrix[currentVexIndex];
+            currentVex.known = true;
             for(const [adjVex, weight] of currentVex.edge){
-                if(currentVex.dist + weight < adjVex.dist){
-                    adjVex.dist = currentVex.dist + weight;
-                    adjVex.path = currentVex;
-                }
-                if(!queue.find((ele)=>ele==adjVex)){ // not in the queue
-                    queue.push(adjVex);
+                if(!adjVex.known){
+                    if(weight < adjVex.dist){
+                        adjVex.dist = weight;
+                        adjVex.path = currentVex;
+                    }
                 }
             }
         }
     }
 
-    printPath(v){// recursive print the path
-        if(v==null)
-            return;
-        this.printPath(v.path);
-        console.log("to");
-        console.log(v.data);
-    }
 }
 
-const values = ['A', 'B', 'C', 'D', 'E'];
+const values = [1,2,3,4,5,6,7];
 // egdes: vex1, vex2, distance
-const edges = [['A','B',210],['A','D',100],['A','E',20],['B','C',20],['B','D',-150],['C','E',10],['D','C',-30],['D','E',60]];
+const edges = [[1,2,2],[2,1,2],[1,4,1],[4,1,1],[1,3,4],[3,1,4],[2,4,3],[4,2,3],[2,5,10],[5,2,10],[4,3,2],[3,4,2],[3,6,5],[6,3,5],[4,5,7],[5,4,7],[4,6,8],[6,4,8],[4,7,4],[7,4,4],[5,7,6],[7,5,6],[7,6,1],[6,7,1]];
 const myGraph = new Graph(values, edges);
-myGraph.weightedNegative(myGraph.adjmatrix[0]); // start point is the vex with value of 1
-myGraph.printPath(myGraph.adjmatrix[4]);
+myGraph.prim(); 
+// console.log(myGraph);
+for(const vertex of myGraph.adjmatrix){
+    if(vertex.path)
+        console.log(vertex.data, "-",vertex.path.data);
+    else
+        console.log(vertex.data, "- null");
+}
