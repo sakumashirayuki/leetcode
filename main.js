2dd8e0a75e9fd3f9ff84c2c6d21b110accbdcff0
sakumashirayuki <2909549511@qq.com>
2021-08-09 15:15:44 +0800

    add 1963

--- main.js
+++ main.js
@@ -1,49 +1,47 @@
-const getEmpty = (o) => {
-    if(Object.prototype.toString.call(o) == '[Object Object]')
-        return {};
-    if(Object.prototype.toString.call(o) == '[Object Array]')
-        return [];
-    return o;
-}
-const deepCopy = (origin) => {
-    const target = getEmpty(origin);
-    const stack = [];//[target, origin]
-    const map = new Map(); // key: origin, value: target
-    if(target!=origin){
-        stack.push([target, origin]);
-        map.set(origin, target);
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var movingCount = function(m, n, k) {
+    let cnt = 0;
+    const vis = new Array(m).fill(null).map(row => new Array(n).fill(false));
+    const digitSum = (x, y) => {
+        let sum = 0;
+        while(x){
+            sum += x % 10;
+            x = Math.floor(x / 10);
+        }
+        while(y){
+            sum += y % 10;
+            y = Math.floor(y / 10);
+        }
+        return sum;
     }
-    while(stack.length){
-        const [t, o] = stack.pop();
-        for(const key in o){
-            if(map.has(key)){
-                t[key] = map.get(key);
-            }else{
-                t[key] = getEmpty(o[key]);
-                if(t[key]!=o[key]){
-                    stack.push([t[key], o[key]]);
-                    map.set(o[key], t[key])
-                }
-            }
+    const dfs = (x, y) => {
+        if(x < 0 || x >= m || y < 0 || y >= n  || vis[x][y])
+            return;
+        const sum = digitSum(x, y);
+        // console.log(x, y, sum, sum > k);
+        if(sum > k){
+            // console.log(x, y, sum, sum > k);
+            vis[x][y] = true;
+            return;
+        }
+        cnt++;
+        vis[x][y] = true;
+        dfs(x - 1, y);
+        dfs(x + 1, y);
+        dfs(x, y - 1);
+        dfs(x, y + 1);
+    }
+    for(let i = 0; i < m; i++){
+        for(let j = 0; j < n; j++){
+            if(!vis[i][j])
+                dfs(i, j);
         }
     }
-    return target
-}
-
-console.log(deepCopy({a:1}));
-console.log(deepCopy([1,2,{a:[3,4]}]))
-console.log(deepCopy(function(){return 1;}))
-console.log(deepCopy({
-    x:function(){
-        return "x";
-    },
-    val:3,
-    arr: [
-        1,
-        {test:1}
-    ]
-}))
-
-let circle = {};
-circle.child = circle;
-console.log(deepCopy(circle));
+    return cnt;
+};
+console.log(movingCount(50, 50, 10));
