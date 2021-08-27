3a0d3bba5c3ced2def4079e6b15c5a680b936113
sakumashirayuki <2909549511@qq.com>
2021-08-27 22:12:08 +0800

    update

--- main.js
+++ main.js
@@ -1,47 +1,33 @@
-/**
- * @param {number} m
- * @param {number} n
- * @param {number} k
- * @return {number}
- */
-var movingCount = function(m, n, k) {
-    let cnt = 0;
-    const vis = new Array(m).fill(null).map(row => new Array(n).fill(false));
-    const digitSum = (x, y) => {
-        let sum = 0;
-        while(x){
-            sum += x % 10;
-            x = Math.floor(x / 10);
+var recoverTree = function(root) {
+    const stack = [];
+    let temp = root;
+    let prev = null;
+    let curr;
+    const disorder = [];
+    while(temp != null || stack.length){
+        while(temp){
+            stack.push(temp)
+            temp = temp.left;
         }
-        while(y){
-            sum += y % 10;
-            y = Math.floor(y / 10);
-        }
-        return sum;
-    }
-    const dfs = (x, y) => {
-        if(x < 0 || x >= m || y < 0 || y >= n  || vis[x][y])
-            return;
-        const sum = digitSum(x, y);
-        // console.log(x, y, sum, sum > k);
-        if(sum > k){
-            // console.log(x, y, sum, sum > k);
-            vis[x][y] = true;
-            return;
-        }
-        cnt++;
-        vis[x][y] = true;
-        dfs(x - 1, y);
-        dfs(x + 1, y);
-        dfs(x, y - 1);
-        dfs(x, y + 1);
-    }
-    for(let i = 0; i < m; i++){
-        for(let j = 0; j < n; j++){
-            if(!vis[i][j])
-                dfs(i, j);
+        // console.log(stack);
+        if(stack.length){
+            temp = stack.pop();
+            // console.log(temp.val);
+            curr = temp;
+            if(prev!=null){
+                if(curr.val < prev.val){
+                    disorder.push(curr);
+                }
+            }
+            prev = curr;
+            temp = temp.right;
         }
     }
-    return cnt;
+    console.log(disorder);
+    const firstNode= disorder[0];
+    const secondNode = disorder[disorder.length - 1];
+    const firstValue = firstNode.val;
+    const secondValue = secondNode.val;
+    firstNode.val = secondValue;
+    secondNode.val = firstValue;
 };
-console.log(movingCount(50, 50, 10));
